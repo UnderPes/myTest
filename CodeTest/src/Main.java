178826ea9438116579dc1673b25a3afd57119809
Oleg Kozlov <grendydaddy@gmail.com>
2024-01-12 22:14:31 +0300

    Initial commit

---
+++ CodeTest/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    static int x =0;
+    static int y =0;
+    public static void main(String[] args) {
+
+        System.out.print(summ(x, y));
+    }
+
+    public static int summ(int x, int y) {
+        return x + y;
+    }
+
+
+}
